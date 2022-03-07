fa55c31a9a00877da3fdb2ca6c8fd1f4e4628205
Akshata72 <sableakshata880@gmail.com>
2022-03-07 19:23:42 +0530

    Added:UC2 logic

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -17,3 +17,11 @@ Console.WriteLine("A Length as 2 Point (x1,y1) & (x2,y2) : " + (x1,y1) + (x2,y2)
 float LengthOfALine =(float)(Math.Sqrt((Math.Pow((x2 - x1), 2)  + Math.Pow((y2 - y1), 2) ))) ;
 Console.WriteLine("Length Of Line :" + LengthOfALine);
 
+if (x1 == x2 )
+{
+    Console.WriteLine("x1 is Equal to x2:" + x1 );
+}
+if (y1 == y2)
+{
+    Console.WriteLine("y1 is Equal to y2:" + y1);
+}
